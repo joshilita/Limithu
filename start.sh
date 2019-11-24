a5478ceaa4cfc9830dc4a806fabff84337406584
joshilita <52619112+joshilita@users.noreply.github.com>
2019-11-24 13:33:12 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+ ACTION='\033[1;90m'
+ FINISHED='\033[1;96m'
+ READY='\033[1;92m'
+ NOCOLOR='\033[0m' # No Color
+ ERROR='\033[0;31m'
 git fetch
  HEADHASH=$(git rev-parse HEAD)
  UPSTREAMHASH=$(git rev-parse master@{upstream})
