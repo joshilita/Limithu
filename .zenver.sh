f892f0d316e971e05b6ffae4f9a6c4957c80b37e
joshilita <52619112+joshilita@users.noreply.github.com>
2019-11-24 14:17:25 -0500

    Create .zenver.sh

---
+++ .zenver.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+if [ $(dpkg-query -W -f='${Status}' dialog 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  echo "Dialog is not installed!"
+  echo ""
+  echo "Installing"
+  apt-get install zenity
+ else
+  dialog --msgbox Dialog is not released yet. 
+fi
