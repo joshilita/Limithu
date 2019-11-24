106ce32b3351cf6f412543c30df758eefd62e523
joshilita <52619112+joshilita@users.noreply.github.com>
2019-11-24 12:14:40 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 echo "Press 'SPACE' to continue."
 while true; do
-read -rsn1 input
-if [ "$input" = "a" ]; then
-    echo "hello world"
-fi
+    read input
+
+    if ["$input" = "a"]; then 
+        echo "hello world"           
+    fi
 done
