a75924bdf1ac185e7bbb3a6e13f8488ae822c3d9
joshilita <52619112+joshilita@users.noreply.github.com>
2019-11-24 12:19:18 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 echo "Press 'SPACE' to continue."
-while true; do
-    read input
-
-    if ["$input" = "a"]; then 
-        echo "hello world"           
-    fi
-done
+read -n1 -r  key
+if [ "$key = '' ];then
+echo "Loading..."
+fi
